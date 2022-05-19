560926333c044042dc8f5e33f5da1d1fa7b0bca9
Bill Ramirez <bilyalon@gmail.com>
2022-05-19 15:54:08 -0600

    Solutions

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(word, time) {
+    text = ''
+    if (time < 0) {
+        return "ERROR"
+    } 
+    else { 
+    for (let i = 0; i < time; i++) {
+        text+=word;
+        }
+    }
+        return text 
 
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
