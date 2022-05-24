f7d1b6622c090e94b6da1212dd01b5aca8d19ac3
Bill Ramírez <bilyalon@gmail.com>
2022-05-24 14:05:31 -0600

    Solutions

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
 const sumAll = function() {
+    if (typeof a !== "number" || typeof b !== "number"){
+        return "ERROR";
 
+    }else if(a < 0 || b < 0 ) {
+        return "ERROR";
+    }
+    else { 
+        let max = Math.max(a,b)
+        let min = Math.min(a,b)
+        let reg = 0;
+         for (let i = min; i <= max; i++){
+            reg += i;         
+        }
+        
+        return reg }
 };
 
 // Do not edit below this line
