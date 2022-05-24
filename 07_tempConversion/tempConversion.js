463fb189dfb4db56250542636177eff8ece83df4
Bill Ramírez <bilyalon@gmail.com>
2022-05-24 16:44:50 -0600

    Solutions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,15 @@
-const ftoc = function() {
+const ftoc = function(f) {
+  reg = (f-32)/1.8;
+  return Math.round(reg * 10)/10;
 
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  reg = c * 1.8 + 32
+  return Math.round(reg * 10)/10;
 };
+
+console.log(ftoc(32));
 
 // Do not edit below this line
 module.exports = {
